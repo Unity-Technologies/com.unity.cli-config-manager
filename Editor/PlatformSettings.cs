e138bdedca201bfda41b1a7868c7d60ec1fa127f
Sean Stolberg <seans@unity3d.com>
2019-09-18 14:32:30 -0700

    Reverting default scripting to Mono for backward compatibility on our existing Jenkins jobs

--- Editor/PlatformSettings.cs
+++ Editor/PlatformSettings.cs
@@ -33,7 +33,7 @@ namespace com.unity.cliconfigmanager
         public bool GraphicsJobs;
         public AndroidSdkVersions MinimumAndroidSdkVersion = AndroidSdkVersions.AndroidApiLevel24;
         public AndroidSdkVersions TargetAndroidSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
-        public ScriptingImplementation ScriptingImplementation = ScriptingImplementation.IL2CPP;
+        public ScriptingImplementation ScriptingImplementation = ScriptingImplementation.Mono2x;
         public string AppleDeveloperTeamId;
         public string IOsProvisioningProfileId;
         public ColorSpace ColorSpace = ColorSpace.Gamma;
