bcf197dbb936e01169376789aa590f2cbc062630
Sean Stolberg <seans@unity3d.com>
2020-03-27 07:41:05 -0700

    Adding capability to collect MSAA from both builtin and SRP. Add more descripting tagging for metadata.

--- Editor/PlatformSettings.cs
+++ Editor/PlatformSettings.cs
@@ -10,6 +10,7 @@ using com.unity.xr.test.runtimesettings;
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.PackageManager;
+using UnityEngine.Rendering.Universal;
 #endif
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -78,6 +79,9 @@ namespace com.unity.cliconfigmanager
             settingsAsset.Username = Username = Environment.UserName;
             settingsAsset.RenderPipeline = RenderPipeline =
                 $"renderpipeline|{(GraphicsSettings.renderPipelineAsset != null ? GraphicsSettings.renderPipelineAsset.name : "BuiltInRenderer")}";
+            settingsAsset.AntiAliasing = GraphicsSettings.renderPipelineAsset != null
+                ? ((UniversalRenderPipelineAsset) GraphicsSettings.renderPipelineAsset).msaaSampleCount
+                : QualitySettings.antiAliasing;
 
 #if OCULUS_SDK
             settingsAsset.StereoRenderingModeDesktop = StereoRenderingModeDesktop.ToString();
@@ -107,7 +111,7 @@ namespace com.unity.cliconfigmanager
 
                 var revision = TryGetRevisionFromPackageJson(xrManagementPackageName) ?? "unavailable";
                 var version = xrManagementPckg.version;
-                packageRevision = string.Format("{0}|{1}|{2}", xrManagementPackageName, version, revision);
+                packageRevision = string.Format("XrManagementPackageName|{0}|XrManagementVersion|{1}|XrManagementRevision|{2}", xrManagementPackageName, version, revision);
             }
 
             return packageRevision;
@@ -156,7 +160,7 @@ namespace com.unity.cliconfigmanager
                     TryGetXrsdkBranch(oculusXrsdkPckg);
                 }
                 packageRevision = string.Format(
-                    "{0}|{1}|{2}|{3}|{4}", 
+                    "XrSdkName|{0}|XrSdkVersion|{1}|XrSdkRevision|{2}|XrSdkRevisionDate|{3}|XrSdkBranch|{4}", 
                     oculusXrSdkPackageName, 
                     version, 
                     XrsdkRevision, 
